4567ee7003986b465d6eee75f402558eb01b7af4
rchaser53 <tayoshizawa29@gmail.com>
2017-04-07 17:48:23 +0900

    working

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,55 +1,91 @@
 import * as React from 'react'
+import {
+    createSelector,
+    createSelectorCreator
+} from 'reselect'
+import { Dispatch } from 'redux'
+import { connect } from 'react-redux'
 
-export class Apps extends React.Component<{}, any> {
+import { InitialState } from '../index'
+
+export interface Props {
+    arr: any[],
+    dispatch: Dispatch<InitialState>
+}
+
+export class Apps extends React.Component<Props, {}> {
     constructor() {
         super()
-
-        this.state = {
-            poyo: ''
-        }
         this.hoge = this.hoge.bind(this)
+        this.noHoge = this.noHoge.bind(this)
     }
 
     hoge() {
-
+        this.props.dispatch({
+            type: 'ADD',
+            payload: Date.now()
+        })
     }
-  render() {
-    return (
-      <div>
-          <div onClick={() => {
-                    console.log('aaa')
-                    this.setState({
-                        poyo: Date.now()
+
+    noHoge() {
+        this.props.dispatch({
+            type: 'NO_ADD'
+        })
+    }
+
+	render() {
+        // console.log(this.props)
+		return (
+			<div>
+				<div onClick={this.hoge}>234</div>
+                <div onClick={this.noHoge}>gya-n</div>
+                {
+                    this.props.arr.map((elem, index) => {
+                        return <TestA key={elem.nyan} val={index} />
                     })
-                }}>234</div>
-                <TestA poyo={this.hoge} />
+                }
 			</div>
 		)
 	}
 }
-export default Apps
+// export default connect((state) => {
+//     console.log('a')
+//     return state
+// })(Apps)
 
+export default connect((state) => {
+    console.log('a')
+    return createSelector<any, any, any>(
+        (state) => {
+            console.log('b')
+            return state
+        },
+        (state) => {
+            console.log('c')
+            return state
+        })
+})(Apps)
 
-class TestA extends React.Component<any, any> {
-    shouldComponentUpdate() {
-        console.log(2143)
-        return false
-    }
+// const customSelectorCreator = createSelectorCreator(
+//     (func, equalityCheck) => {
+//         let lastArgs = null
+//         let lastResult = null
 
+//         return () => {
+//             if (!areArgumentsShallowlyEqual(equalityCheck, lastArgs, arguments)) {
+//                 lastResult = func.apply(null, arguments)
+//             }
+//             lastArgs = arguments
+//             return lastResult
+//         }
+//     }
+// )
+
+class TestA extends React.Component<{val: number}, {}> {
     render() {
-        console.log('testA')
+        console.log(this.props.val)
         return  <div>
-                    <div>testA</div>
-                    <TestB />
-                </div>
-    }
-}
-
-class TestB extends React.PureComponent<any, any> {
-    render() {
-        console.log('testB')
-        return  <div>
-                    <div>testB</div>
+                    <div>{this.props.val}</div>
                 </div>
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,37 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './component/App'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+import {
+    Provider
+} from 'react-redux'
+
+import { createStore } from 'redux'
+
+export interface InitialState {
+    arr: any[]
+}
+
+export const initialState: InitialState = {
+    arr: []
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD':
+            return { arr: state.arr.concat({ nyan:  action.payload }) }
+        case 'NO_ADD':
+            // const temp = 
+            // console.log(state.arr)
+            return { arr: state.arr.concat()}
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer, initialState)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.querySelector('#root'))
